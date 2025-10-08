b327a26f512ce0e285a7b36fdcd2c21af0bfddf8
nganne <thanhngan.pt2004@gmail.com>
2025-10-08 09:08:52 +0700

    Fix call api from backend

--- src/main/java/todo/list/nganmtt/configuration/WebConfig.java
+++ src/main/java/todo/list/nganmtt/configuration/WebConfig.java
@@ -1,5 +1,7 @@
 package todo.list.nganmtt.configuration;
 
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,12 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.http.MediaType;
 
 @Configuration
+@RequiredArgsConstructor
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
 public class WebConfig implements WebMvcConfigurer {
+
+    AppProperties appProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
+                .allowedOrigins(
+                        appProperties.getBackendUrl()
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
