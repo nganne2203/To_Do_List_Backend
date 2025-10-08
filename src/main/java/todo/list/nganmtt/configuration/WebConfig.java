1043ab4f0bc967c81f86fa735e426075820b8203
nganne <thanhngan.pt2004@gmail.com>
2025-10-08 10:48:55 +0700

    Fix 400 swagger

--- src/main/java/todo/list/nganmtt/configuration/WebConfig.java
+++ src/main/java/todo/list/nganmtt/configuration/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
+                .allowedOriginPatterns(
                         appProperties.getBackendUrl(),
                         appProperties.getFrontendUrl()
                 )
